1844d7b7f942a2a19e57cbb43abed117dba57325
daniel-takacs <takacsd89@gmail.com>
2021-01-21 22:05:58 +0200

    3 files modified, 6 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./style.css";
-
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' />
+      </Switch>
+    </Router>
     </div>
   );
 }
---
+++ src/components/SidebarData.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+import { AiFillHome } from "react-icons/ai";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiFillHome />,
+    cName: "nav-text"
+  },
+  {
+    title: "Reports",
+    path: "/",
+    icon: <AiFillHome />,
+    cName: "nav-text"
+  },
+  {
+    title: "Products",
+    path: "/",
+    icon: <AiFillHome />,
+    cName: "nav-text"
+  }
+];
